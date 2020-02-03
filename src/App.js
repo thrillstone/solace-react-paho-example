d5de019173090882d01aa632900d474cd41d4c8a
Kevin Lidstone <kevin.lidstone@solace.com>
2020-02-02 19:03:15 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import messaging from "./Messaging";
+import Paho from "paho-mqtt";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			connected: false,
+			messages: []
+		};
+		messaging.register(this.handleMessage.bind(this));
+	}
+
+	render() {
+		const connected = this.state.connected;
+		const sendButton = connected ? <button onClick={() => this.handleSendClick()}>Send</button> : <button disabled>Send</button>;
+		return (
+			<div className="App">
+				<div class="buttons">
+					<button onClick={() => this.handleConnectClick()}>{connected ? 'Disconnect' : 'Connect'}</button>
+					{sendButton}
+				</div>
+				<ol>
+					{this.state.messages.map((message, index) => {
+						return <li key={index}>{message}</li>
+					})}
+				</ol>
+			</div>
+		  );
+	}
+
+	handleMessage(message) {
+		this.setState(state => {
+			const messages = state.messages.concat(message.payloadString);
+			return {
+				messages,
+				connected: state.connected,
+			};
+		  });
+	}
+
+	handleSendClick() {
+		let message = new Paho.Message(JSON.stringify({text: "Hello"}));
+		message.destinationName = "exampletopic";
+		messaging.send(message);
+	}
+
+	handleConnectClick() {
+		if (this.state.connected) {
+			messaging.disconnect();
+			this.setState({
+				connected: false,
+				messages: this.state.messages
+			});
+		} else {
+			messaging.connectWithPromise().then(response => {
+				console.log("Succesfully connected to Solace Cloud.", response);
+				messaging.subscribe("exampletopic");
+				this.setState({
+					connected: true,
+					messages: this.state.messages
+				});
+			}).catch(error => {
+				console.log("Unable to establish connection with Solace Cloud, see above logs for more details.", error);
+			});
+		}
+	}
+
 }
+
+
 
 export default App;
---
+++ src/messaging-options.js
@@ -0,0 +1,14 @@
+export default {
+	userName: "solace-cloud-client",
+	password: "19dfphtbfnmt8vmgnj05i5844i",
+	invocationContext: {
+		host: "mr1u6o37qn55wj.messaging.solace.cloud",
+		port: 8443,
+		clientId: "react-app"
+	},
+	timeout: 3,
+	keepAliveInterval: 60,
+	cleanSession: true,
+	useSSL: true,
+	reconnect: true
+};
